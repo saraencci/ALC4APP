330e6c73aa5391660175d9610a0f07e4b1e3160a
saraencci <franksaraencci@gmail.com>
2019-07-14 09:38:16 +0300

    deleted test activities

--- app/src/main/java/com/saraencci/alc4app/profileActivity.java
+++ app/src/main/java/com/saraencci/alc4app/profileActivity.java
@@ -3,17 +3,29 @@ package com.saraencci.alc4app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.DisplayCutout;
 
 public class profileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle("My Profile");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            DisplayCutout displayCutout = getWindow().getDecorView().getRootWindowInsets().getDisplayCutout();        }
         setContentView(R.layout.activity_profile);
         assert getSupportActionBar() != null;   //null check
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTitle("My Profile");
+
+
+
+
+
+
+
+
 
     }
     @Override
